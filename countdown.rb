d2634dd87253c94ada1a9cd863e9c28d3017f91a
Toni Lamont <tlam212@gmail.com>
2019-04-16 15:22:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  counter = 0
+  
+  while counter >= num
+  counter -= 1 
+  puts "#{num} SECOND(S)!"
+  break if counter == 1
+end
+  puts "HAPPY NEW YEAR!"
+  
 end
